4be5496138ec2656d0fa0ebd67c2b01c3c22a92d
Anusha Shekhar <anushashekhar@Anushas-MacBook-Pro.local>
2023-09-24 23:23:19 -0400

    Activity 4.1: hello <name> application using form with required field

--- README.md
+++ README.md
@@ -7,8 +7,11 @@ Below is a screenshot proving that I have successfully installed Flask in a virt
 
 ## Activity 2
 Below is a screenshot of my 'hello' HTML page with my name on the web page.
-![activity 2: hello Anusha](./images/lactivity2.png)
+![activity 2: hello Anusha](./images/activity2.png)
 
 ## Activity 3
 Below is a screenshot showing that I have successfully finished the task of Activity 3.
 ![activity 3: hello Anusha with a timestamp](./images/activity3.png)
+**NOTE:** you will see a number of "fix images" commits after the commits for Activity 3. These were done as I was running into issues with the images. I initially uploaded an incorrect picture for the Activity 3 image, but when I modified it and re-committed, the image was not updating on GitHub (attempted twice). Hence, I decided to rename all the images and re-commit and re-push, which fixed the error.
+
+## Activity 4
--- hello.py
+++ hello.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'ece444_lab1_wtf_config'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST']) 
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!') 
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form, name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -22,3 +35,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
